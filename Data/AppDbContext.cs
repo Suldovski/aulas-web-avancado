1f71b5dd0366dfd9629b2bcf58d4eb5d6b3aac88
João Paulo Nunes da Silva <nunes0188@outlook.com.br>
2026-03-11 19:40:39 -0300

    Criação

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameWorkCore;
+
+namespace Revisao{
+
+    public class AppDbContext : DbContext{
+        //DbSet indica que Produto será uma tabela
+        public DbSet<Produto> Produtos = Set<Produto>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            if (!optionsBuilder.IsConfigured){
+                var connectionString = 
+                "server=localhost;database=revisao;user=root;password=";
+
+                optionsBuilder.UseMysql(
+                    connectionString,
+                    ServerVersion.AutoDetect(connectionString)
+                );
+            }
+        }
+
+
+    }
+}
