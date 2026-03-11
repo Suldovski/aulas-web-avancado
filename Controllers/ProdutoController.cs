65fd3bb4982b089c31ecf22dc9829716e24623a1
LUAN GABRIEL ILIANO SULDOVSKI <luan.suldovski@cs.up.edu.br>
2026-03-11 20:21:55 -0300

    intervalo 11.03

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -2,6 +2,33 @@ namespace Revisao
 {
     public class ProdutoController
     {
-       private readonly  
+        private readonly AppDbContext _context;
+
+        public ProdutoController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Produto CriarProduto(string nome,
+        decimal preco, int estoque)
+        {
+            var produto = new Produto
+            {
+                Nome = nome,
+                Preco = preco,
+                Estoque = estoque
+            };
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            return produto;
+        }
+        public List<Produto> ListarProdutos()
+        {
+            //AsNoTracking() não é rastreado pelo Entity Framework, melhorando a performance
+            return _context.Produtos
+            .AsNoTracking()
+            .OrderBy(p => p.Id)
+            .Tolist();
+        }
+
     }
 }
